cebab6879e7250c5885af7c63a984b180f82a4e8
GUA19 <gua@hashwave.io>
2022-11-15 22:22:32 -0500

    implement test get and post api

--- server/index.js
+++ server/index.js
@@ -5,8 +5,19 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname, "/public")))
 
-app.get('*', (_req, res) => {
+app.get('/', (_req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+})
+
+app.get('/test', async (req, res) => {
+    res.send('HI')
+    return
+})
+
+app.post('/test', async (req, res) => {
+    console.log(req.body.data)
+    res.status(200).send();
+    return
 })
 
 app.listen(port, () => {
