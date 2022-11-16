23adeea78a0087581da98f0b0fd38c802f459091
GUA19 <gua@hashwave.io>
2022-11-16 10:41:18 -0500

    update .gitingore

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const path = require("path");
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, "/public")))
+
+// body parser for application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', (_req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
@@ -15,7 +19,7 @@ app.get('/test', async (req, res) => {
 })
 
 app.post('/test', async (req, res) => {
-    console.log(req.body.data)
+    console.log("fsr0:", req.body.fsr0, " fsr1", req.body.fsr1, " fsr2", req.body.fsr2, " fsr3", req.body.fsr3, " tmp", req.body.temp);
     res.status(200).send();
     return
 })
